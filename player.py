d2ae01075201b9df255bdbed90afbf448e7871ad
Benjamin Kovacs <kojokobe@gmail.com>
2019-05-27 11:23:06 +0200

    call everything

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ class Player:
     VERSION = "Fantastic4"
 
     def betRequest(self, game_state):
-        return int(self.find_biggest_bet(game_state["players"])) + 1
+        return int(game_state['current_buy_in']) - int(game_state['players'][game_state['in_action']]['bet'])
 
     def showdown(self, game_state):
         pass
@@ -15,37 +15,82 @@ class Player:
 
 if __name__ == '__main__':
     game_state = {
-      "players":[
-        {
-          "name":"Player 1",
-          "stack":1000,
-          "status":"active",
-          "bet":23,
-          "hole_cards":[],
-          "version":"Version name 1",
-          "id":0
-        },
-        {
-          "name":"Player 2",
-          "stack":1000,
-          "status":"active",
-          "bet":0,
-          "hole_cards":[],
-          "version":"Version name 2",
-          "id":1
-        }
-      ],
-      "tournament_id":"550d1d68cd7bd10003000003",
-      "game_id":"550da1cb2d909006e90004b1",
-      "round":0,
-      "bet_index":0,
-      "small_blind":10,
-      "orbits":0,
-      "dealer":0,
-      "community_cards":[],
-      "current_buy_in":0,
-      "pot":0
-    }
+  "tournament_id":"550d1d68cd7bd10003000003",
+
+  "game_id":"550da1cb2d909006e90004b1",
+
+  "round":0,
+  "bet_index":0,
+  "small_blind": 10,
+  "current_buy_in": 320,
+  "pot": 400,
+  "minimum_raise": 240,
+  "dealer": 1,
+  "orbits": 7,
+  "in_action": 1,
+  "players": [
+      {
+
+          "id": 0,                                # Id of the player (same as the index)
+
+          "name": "Albert",                       # Name specified in the tournament config
+
+          "status": "active",                     # Status of the player:
+                                                  #   - active: the player can make bets, and win the current pot
+                                                  #   - folded: the player folded, and gave up interest in
+                                                  #       the current pot. They can return in the next round.
+                                                  #   - out: the player lost all chips, and is out of this sit'n'go
+
+          "version": "Default random player",     # Version identifier returned by the player
+
+          "stack": 1010,                          # Amount of chips still available for the player. (Not including
+                                                  #     the chips the player bet in this round.)
+
+          "bet": 320                              # The amount of chips the player put into the pot
+      },
+      {
+          "id": 1,                                # Your own player looks similar, with one extension.
+          "name": "Bob",
+          "status": "active",
+          "version": "Default random player",
+          "stack": 1590,
+          "bet": 80,
+          "hole_cards": [                         # The cards of the player. This is only visible for your own player
+                                                  #     except after showdown, when cards revealed are also included.
+              {
+                  "rank": "6",                    # Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
+                  "suit": "hearts"                # Suit of the card. Possible values are: clubs,spades,hearts,diamonds
+              },
+              {
+                  "rank": "K",
+                  "suit": "spades"
+              }
+          ]
+      },
+      {
+          "id": 2,
+          "name": "Chuck",
+          "status": "out",
+          "version": "Default random player",
+          "stack": 0,
+          "bet": 0
+      }
+  ],
+  "community_cards": [                            # Finally the array of community cards.
+      {
+          "rank": "4",
+          "suit": "spades"
+      },
+      {
+          "rank": "A",
+          "suit": "hearts"
+      },
+      {
+          "rank": "6",
+          "suit": "clubs"
+      }
+  ]
+}
 
     p = Player()
     print(p.betRequest(game_state))
